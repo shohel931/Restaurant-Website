8da66f7c7d2a61cd50e29254dc70a8de2aa0e2c8
MD. Shohel Rana <shohel065931@gmail.com>
2025-05-26 10:48:02 +0600

    ryt

--- 404.php
+++ 404.php
@@ -1,3 +1,8 @@
 <section id="not_found">
-    
+    <div class="not_found_page">
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <a href="<?php echo home_url(); ?>"> Back Home</a>
+    </div>
 </section>
